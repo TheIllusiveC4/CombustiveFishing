d0d7b6e26e7f15f022ebbc3164b60a01669d880d
TheIllusiveC4 <itwasmec4@gmail.com>
2019-04-04 23:10:52 -0700

    combustive cod can swim now

--- src/main/java/top/theillusivec4/combustivefishing/common/entity/EntityLavaMob.java
+++ src/main/java/top/theillusivec4/combustivefishing/common/entity/EntityLavaMob.java
@@ -3,6 +3,7 @@ package top.theillusivec4.combustivefishing.common.entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.EntityWaterMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
@@ -11,6 +12,10 @@ public abstract class EntityLavaMob extends EntityWaterMob {
     protected EntityLavaMob(EntityType<?> type, World world) {
         super(type, world);
         this.isImmuneToFire = true;
+        this.setPathPriority(PathNodeType.WATER, -1.0F);
+        this.setPathPriority(PathNodeType.LAVA, 8.0F);
+        this.setPathPriority(PathNodeType.DANGER_FIRE, 0.0F);
+        this.setPathPriority(PathNodeType.DAMAGE_FIRE, 0.0F);
     }
 
     @Override
