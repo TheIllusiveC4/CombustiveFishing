898a22e3aeb8e03774d06a33870f6fb48745a3a3
TheIllusiveC4 <itwasmec4@gmail.com>
2019-04-27 19:04:35 -0700

    add copyright

--- src/main/java/top/theillusivec4/combustivefishing/common/item/ItemBoneFish.java
+++ src/main/java/top/theillusivec4/combustivefishing/common/item/ItemBoneFish.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2017-2019  C4
+ *
+ * This file is part of Combustive Fishing, a mod made for Minecraft.
+ *
+ * Combustive Fishing is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Combustive Fishing is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Combustive Fishing.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.theillusivec4.combustivefishing.common.item;
 
 import com.google.common.collect.Lists;
